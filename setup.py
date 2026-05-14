6c656301a4c0b569752337bb4a3291ebc735e81a
lvancer <lin029011@163.com>
2026-05-14 11:39:50 +0800

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zign",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/lvancer/zign",
     author="lvancer",
     author_email="lin029011@163.com",
